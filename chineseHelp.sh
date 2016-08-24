32499766a97ab4ef3017f8c40bff7f9cdb9e2b40
sammy <dc517@163.com>
2016-08-24 09:41:01 +0800

    vim中文导入

---
+++ chineseHelp.sh
@@ -0,0 +1,6 @@
+wget http://nchc.dl.sourceforge.net/sourceforge/vimcdoc/vimcdoc-1.5.0.tar.gz
+tar -xvf vimcdoc-1.5.0.tar.gz 
+cd  vimcdoc-1.5.0
+./vimcdoc.sh -i
+cd ..
+rm -rf vimcdoc-1.5.0.tar.gz vimcdoc-1.5.0
